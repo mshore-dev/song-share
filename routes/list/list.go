8d6f5c072a5670c71ad5d574928cada6addca6f5
mshore-dev <iam@mshore.dev>
2024-06-25 21:45:01 -0400

    improvements to list page, now renders properly on screens of differing widths

--- routes/list/list.go
+++ routes/list/list.go
@@ -23,7 +23,42 @@ func RegisterRoutes(app *fiber.App) {
 func routeGetList(c *fiber.Ctx) error {
 
 	c.Render("list", fiber.Map{
-		"title": "Demo List",
+		"title":       "Demo List",
+		"description": "A really cool demo playlist!",
+		"songs": []fiber.Map{
+			{
+				"link":          "https://www.youtube.com/watch?v=3nlSDxvt6JU",
+				"title":         "Snail's House - Pixel Galaxy (Official MV)",
+				"comment":       "A really cool song I liked!",
+				"created_at":    "11:09 PM - 1 Jan 2016",
+				"score":         7,
+				"comment_count": 1,
+			},
+			{
+				"link":          "https://www.youtube.com/watch?v=uZf8a_tE00Q",
+				"title":         "Pixel Fantasy - Pixel Galaxy (Official MV)",
+				"comment":       "Another catchy song!",
+				"created_at":    "12:09 PM - 2 Jan 2016",
+				"score":         8,
+				"comment_count": 2,
+			},
+			{
+				"link":          "https://www.youtube.com/watch?v=n4U20g40jZc",
+				"title":         "Supercell - Snowflakes (Official MV)",
+				"comment":       "A beautiful song!",
+				"created_at":    "1:09 PM - 3 Jan 2016",
+				"score":         -2,
+				"comment_count": 3,
+			},
+			{
+				"link":          "https://www.youtube.com/watch?v=a_uG_h0v34o",
+				"title":         "Pixel Heart - Pixel Galaxy (Official MV)",
+				"comment":       "A touching song!",
+				"created_at":    "2:09 PM - 4 Jan 2016",
+				"score":         10,
+				"comment_count": 4,
+			},
+		},
 	})
 
 	return nil
